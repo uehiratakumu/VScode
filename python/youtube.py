e6a48919240213d847821cbc9dbab982af7c054c
uehiratakumu <takumu1623@icloud.com>
2023-10-26 15:57:09 +0900

    main

--- python/youtube.py
+++ python/youtube.py
@@ -2,5 +2,5 @@ from yt_dlp import YoutubeDL
  
 ydl_opts = {'format': 'best'}
 with YoutubeDL(ydl_opts) as ydl:
-    result = ydl.download(['https://www.youtube.com/watch?v=qU7B_sxRZ-8'])
+    result = ydl.download(['https://www.youtube.com/watch?v=eALcAWaJRbc'])
     print(result)
