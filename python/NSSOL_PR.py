6713f8a84c32567aeb37235d89aca75bab2a8935
uehiratakumu <takumu1623@icloud.com>
2023-11-28 11:10:13 +0900

    main

--- python/NSSOL_PR.py
+++ python/NSSOL_PR.py
@@ -8,6 +8,17 @@ soup = BeautifulSoup(r.content, 'lxml') # または、'html.parser'
 selector ="#body > div.v2-main-column > div.v2-company-experiences-list > div > div:nth-child(2) > a > div > div.v2-experiences__text-summary"
 
 s = soup.select_one(selector).get_text() 
+path = 'NSSOL_PR.txt'
+with open(path, mode='r') as f:
+    t =f.read()
+    if s == t:
+        k = "更新なし"
+    else:
+        k = "更新あり:https://www.onecareer.jp/companies/523/experiences"
+        
+if s  != t:
+    with open(path, mode='w') as f:
+        f.write(s)
 
 import requests
 
@@ -15,6 +26,6 @@ import requests
 TOKEN = "JEf8loIMcsRM87uPfElmVtWVAC9kFlbPdzVXCvYoKMj"
 
 headers = {'Authorization': f'Bearer {TOKEN}'}
-data = {'message': s}
+data = {'message': k}
 
 response = requests.post('https://notify-api.line.me/api/notify', headers=headers, data=data)
